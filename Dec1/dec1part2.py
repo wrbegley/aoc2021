74d303ae236a130ae2af70e87c961dae65dc189f
wrbegley <richard@richardbegley.com>
2021-12-01 01:22:48 -0500

    update after part one complete

---
+++ Dec1/dec1part2.py
@@ -0,0 +1,28 @@
+# Advent of Code 2021 Day 1
+# Richard Begley
+# December 1, 2021
+#
+
+depth = []
+count = 0
+idx = 0
+with open("Dec1Inputpart1.txt", "r") as depths:
+    for depthvalue in depths:
+        depthvalue = int(depthvalue.strip())
+        depth.append(depthvalue)
+        depthlistlength = len(depth)
+
+for x in range(depthlistlength):
+
+    if idx < depthlistlength - 1:
+        # print(depth[idx])
+        # print(depth[idx + 1])
+        depth1 = depth[idx]
+        depth2 = depth[idx + 1]
+        if depth1 < depth2:
+            count +=1
+        else:
+            pass
+        idx += 1
+    else:
+        print(count)
